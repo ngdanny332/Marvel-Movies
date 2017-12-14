1ac602b22a134dc27c7db28e1328a386c6821bee
ngdanny332 <34078460+ngdanny332@users.noreply.github.com>
2017-12-13 20:40:32 -0800

    Add files via upload

---
+++ Marvel.py
@@ -0,0 +1,86 @@
+import fresh_tomatoes
+import media
+
+CaptainAmerica1 = media.Movie("The First Avenger",
+                              "The birth of a super solider during the World War",
+                              "https://images-na.ssl-images-amazon.com/images/I/91zlVmXTLOL._SY445_.jpg",
+                              "https://www.youtube.com/watch?v=JerVrbLldXw")
+IronMan = media.Movie ("Iron Man",
+                       "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+                       "https://images-na.ssl-images-amazon.com/images/I/91qvAndeVYL._RI_.jpg",
+                       "https://www.youtube.com/watch?v=8hYlB38asDY")
+
+TheIncredibleHulk = media.Movie("The Incredible Hulk",
+                                "Bruce Banner, a scientist on the run from the U.S. Government, must find a cure for the monster he turns into, whenever he loses his temper.",
+                                "https://images-na.ssl-images-amazon.com/images/I/91wFHajfFpL._RI_.jpg",
+                                "https://www.youtube.com/watch?v=xbqNb2PFKKA")
+
+IronMan2 = media.Movie ("Iron Man 2",
+                        "With the world now aware of his identity as Iron Man, Tony Stark must contend with both his declining health and a vengeful mad man with ties to his father's legacy.",
+                        "https://images-na.ssl-images-amazon.com/images/I/91UgoL1eiUL._SL1500_.jpg",
+                        "https://www.youtube.com/watch?v=BoohRoVA9WQ")
+
+Thor = media.Movie("Thor",
+                   "The powerful, but arrogant god Thor, is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.",
+                   "http://cdn.collider.com/wp-content/uploads/thor-movie-poster-chris-hemsworth-01.jpg",
+                   "https://www.youtube.com/watch?v=JOddp-nlNvQ")
+
+IronMan3 = media.Movie ("Iron Man 3",
+                        "When Stark finds his personal world destroyed at his enemy's hands, he embarks on a harrowing quest to find those responsible.",
+                        "https://upload.wikimedia.org/wikipedia/en/d/d5/Iron_Man_3_theatrical_poster.jpg",
+                        "https://www.youtube.com/watch?v=YLorLVa95Xo")
+
+TheDarkWorld = media.Movie ("Thor: The Dark World",
+                                "When Dr. Jane Foster gets cursed with a powerful entity known as the Aether, Thor is heralded of the cosmic event known as the Convergence and the genocidal Dark Elves",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BOTQ0MjcwODk5NV5BMl5BanBnXkFtZTgwMjQ0OTIxMDE@._V1_SY1000_CR0,0,625,1000_AL_.jpg",
+                                "https://www.youtube.com/watch?v=npvJ9FTgZbM")
+
+TheWinterSoldier = media.Movie ("The Winter Soldier",
+                                              "As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat from history: an assassin known as the Winter Soldier.",
+                                              "https://upload.wikimedia.org/wikipedia/en/e/e8/Captain_America_The_Winter_Soldier.jpg",
+                                              "https://www.youtube.com/watch?v=tbayiPxkUMM")
+
+GuardiansOfTheGalaxy = media.Movie ("Guardians of The Galaxy",
+                                    "A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.",
+                                    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+                                    "https://www.youtube.com/watch?v=d96cjJhvlMA")
+
+GuardiansOfTheGalaxy2 = media.Movie ("Guardians of The Galaxy",
+                                     "The Guardians must fight to keep their newfound family together as they unravel the mystery of Peter Quill's true parentage.",
+                                     "https://images-na.ssl-images-amazon.com/images/I/61AZ0FAdLvL._SX342_.jpg",
+                                     "https://www.youtube.com/watch?v=dW1BIid8Osg")
+
+AgeOfUltron = media.Movie ("Avengers: Age of Ultron",
+                                   "When Tony Stark and Bruce Banner try to jump-start a dormant peacekeeping program called Ultron, things go horribly wrong and it's up to Earth's mightiest heroes to stop the villainous Ultron from enacting his terrible plan.",
+                                   "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",
+                                   "https://www.youtube.com/watch?v=tmeOjFno6Do")
+
+AntMan = media.Movie ("Ant-Man",
+                      "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, plan and pull off a heist that will save the world.",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_UY1200_CR89,0,630,1200_AL_.jpg",
+                      "https://www.youtube.com/watch?v=pWdKf3MneyI")
+
+CivilWar = media.Movie ("Civil War",
+                        "Political interference in the Avengers' activities causes a rift between former allies Captain America and Iron Man.",
+                        "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
+                        "https://www.youtube.com/watch?v=dKrVegVI0Us")
+
+DoctorStrange = media.Movie ("Doctor Strange",
+                             "While on a journey of physical and spiritual healing, a brilliant neurosurgeon is drawn into the world of the mystic arts.",
+                             "https://upload.wikimedia.org/wikipedia/en/c/c7/Doctor_Strange_poster.jpg",
+                             "https://www.youtube.com/watch?v=HSzx-zryEgM")
+
+Homecoming = media.Movie ("Spider-Man: Homecoming",
+                          "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.",
+                          "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg",
+                          "https://www.youtube.com/watch?v=39udgGPyYMg")
+
+Ragnarok = media.Movie ("Thor: Ragnarok",
+                        "Thor must fight for survival and race against time to prevent the all-powerful Hela from destroying his home and the Asgardian civilization.",
+                        "https://d1mm3624z6jafc.cloudfront.net/movies/posters/ThorRagnarok_PayoffOneSheet_R.JPG?v=13",
+                        "https://www.youtube.com/watch?v=ue80QwXMRHg&t=6s")
+
+movies = [CaptainAmerica1, IronMan, TheIncredibleHulk, IronMan2, Thor, IronMan3, TheDarkWorld, TheWinterSoldier, GuardiansOfTheGalaxy, GuardiansOfTheGalaxy2, AgeOfUltron, AntMan, CivilWar, DoctorStrange, Homecoming, Ragnarok]
+fresh_tomatoes.open_movies_page(movies)
+
+print (media.Movie.__doc__)

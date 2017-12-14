4cb94c4d76a907b1eaf67a8577fed66cffe993b4
ngdanny332 <34078460+ngdanny332@users.noreply.github.com>
2017-12-14 12:43:05 -0800

    Update media.py

--- media.py
+++ media.py
@@ -1,17 +1,37 @@
+"""
+Module used to display movie object, attributes and instances
+"""
+
 import webbrowser
 
+
 class Movie():
-    """This class provides a way to store  movie related information"""
+    """
+Class object stores movie related information
+    """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-# Create data for the movie
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube):
+        """
+        Initialize instances of class Movie
+        :param movie_title: string
+        :param movie_storyline: string
+        :param poster_image: string
+        :param trailer_youtube: string
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-# Make the trailer open a youtube link
-
     def show_trailer(self):
+        """
+        Initializing instance for opening the youtube video
+
+        :return: webbrowser to play movie
+        """
         webbrowser.open(self.trailer_youtube_url)
